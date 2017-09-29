ba8517f9276b7e13e2e4af9176aac771d5c0606c
Diego Spinedi <diego.spinedi@cpba.com.ar>
2017-09-29 13:24:25 -0300

    Commit

--- PruebaJenkins/Form1.vb
+++ PruebaJenkins/Form1.vb
@@ -3,6 +3,9 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         MsgBox("Prueba")
+        MsgBox("Prueba")
+
+        Me.Focus.ToString()
 
     End Sub
 End Class
